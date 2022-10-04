c6b556f1b597ce6b2333f93a8b8ecd1db70e0fe8
Renato Guedes <rmguedes@hotmail.com>
2022-10-04 12:34:47 -0300

    Change layout direction on projects at mobile version and small details

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,29 @@
 const slider = document.querySelector(".project-slider");
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 40,
-  freeMode: true,
-  grabCursor: true,
-});
+if (window.innerWidth > 650) {
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    freeMode: true,
+    grabCursor: true,
+  });
+}
 
-if (slider) {
+if (window.innerWidth <= 650) {
+  let swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 10,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+
+if (slider && window.innerWidth > 650) {
   slider.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     slider.scrollLeft += evt.deltaY;
